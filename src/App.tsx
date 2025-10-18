ad4325c1bd436e8dc2878a02a33ca88c41745d57
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2025-10-18 15:49:05 +0200

    Add 2 timers (with not so precise 1s setInterval approach)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    // const [isRunning, setIsRunning] = useState(false)
+    const [activePlayer, setActivePlayer] = useState(1)
+    const [playerTimer1, setPlayerTimer1] = useState(5)
+    const [playerTimer2, setPlayerTimer2] = useState(5)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        const intervalId: NodeJS.Timeout = setInterval(() => {
+            if (activePlayer === 1) {
+                setPlayerTimer1(previousTime => previousTime - 1)
+            } else {
+                setPlayerTimer2(previousTime => previousTime - 1)
+            }
+        }, 1000)
+
+        return () => {
+            if (intervalId) {
+                clearInterval(intervalId)
+            }
+        }
+    }, [activePlayer]);
+
+    const setCurrentActivePlayer = () => {
+        const currentPlayer = 1 + (activePlayer % 2)
+        console.log(currentPlayer)
+        setActivePlayer(currentPlayer);
+    }
+
+    return (
+        <>
+            <h1>{playerTimer1}</h1>
+            <h1>{playerTimer2}</h1>
+            <button onClick={setCurrentActivePlayer}>Correct answer!</button>
+        </>
+    )
 }
 
 export default App
