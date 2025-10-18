511e82de04f695da537c3fc5f61dffd7de23d8d2
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2025-10-18 21:55:44 +0200

    Basic timers logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,74 @@
 import {useEffect, useState} from 'react'
 import './App.css'
 
+
 function App() {
-    // const [isRunning, setIsRunning] = useState(false)
+    const TIME_SECONDS = 5;
+
+    const [isRunning, setIsRunning] = useState(false)
+    const [isGameFinished, setIsGameFinished] = useState(false)
     const [activePlayer, setActivePlayer] = useState(1)
-    const [playerTimer1, setPlayerTimer1] = useState(5)
-    const [playerTimer2, setPlayerTimer2] = useState(5)
+    const [playerTimer1, setPlayerTimer1] = useState(TIME_SECONDS)
+    const [playerTimer2, setPlayerTimer2] = useState(TIME_SECONDS)
+
+    function checkAndFinishGame(previousTime: number): number {
+        if (previousTime <= 1) {
+            setIsRunning(false)
+            setIsGameFinished(true)
+            setPlayerTimer1(TIME_SECONDS)
+            setPlayerTimer2(TIME_SECONDS)
+            return 0
+        }
+        return previousTime - 1
+    }
 
     useEffect(() => {
-        const intervalId: NodeJS.Timeout = setInterval(() => {
-            if (activePlayer === 1) {
-                setPlayerTimer1(previousTime => previousTime - 1)
-            } else {
-                setPlayerTimer2(previousTime => previousTime - 1)
-            }
-        }, 1000)
+        if (isRunning) {
+            const intervalId: NodeJS.Timeout = setInterval(() => {
+                if (activePlayer === 1) {
+                    setPlayerTimer1(previousTime => checkAndFinishGame(previousTime))
+                } else {
+                    setPlayerTimer2(previousTime => checkAndFinishGame(previousTime))
+                }
+            }, 1000)
 
-        return () => {
-            if (intervalId) {
-                clearInterval(intervalId)
+            return () => {
+                if (intervalId) {
+                    clearInterval(intervalId)
+                }
             }
         }
-    }, [activePlayer]);
+    }, [activePlayer, isRunning]);
 
-    const setCurrentActivePlayer = () => {
-        const currentPlayer = 1 + (activePlayer % 2)
+    const setNextActivePlayer = () => {
+        const currentPlayer = getNextActivePlayer()
         console.log(currentPlayer)
         setActivePlayer(currentPlayer);
     }
 
+    const getNextActivePlayer = (): number => 1 + (activePlayer % 2)
+
+    const startGame = () => {
+        setIsGameFinished(false)
+        setIsRunning(true)
+    }
+
     return (
         <>
-            <h1>{playerTimer1}</h1>
-            <h1>{playerTimer2}</h1>
-            <button onClick={setCurrentActivePlayer}>Correct answer!</button>
+            {isGameFinished ?
+                <h1>Player {getNextActivePlayer()} won!</h1>
+                :
+                <>
+                    <h1>{playerTimer1}</h1>
+                    <h1>{playerTimer2}</h1>
+                </>
+            }
+            {!isRunning ?
+                <button onClick={startGame}>Start game =)</button>
+                :
+                <button onClick={setNextActivePlayer}>Correct answer!</button>
+            }
+
         </>
     )
 }
