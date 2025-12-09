3b025d1e3cd88765c883982d4dac202cc30aa142
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2025-12-09 22:12:57 +0100

    Basic config

--- src/components/gamescreen/question/questionUtils.ts
+++ src/components/gamescreen/question/questionUtils.ts
@@ -1,5 +1,5 @@
 export const getImageFromCategory = (category: string, index: number): string => {
-    return `./categories/${category}/${index}.jpg`
+    return `/categories/${category}/${index}.jpg`
 };
 
 // export const getTextFromCategory = (category: string, index: number): string => {
