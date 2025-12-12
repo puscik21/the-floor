b31c6b754658af4da131dce5fdd20b6cc71199ee
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2025-12-12 16:20:03 +0100

    Fix browser crash bug

--- src/components/gamescreen/ActionsSection.tsx
+++ src/components/gamescreen/ActionsSection.tsx
@@ -69,29 +69,41 @@ const Container = styled('footer')`
     display: flex;
     align-items: center;
     justify-content: center;
-    width: 100%;
+    width: 50%;
+
+    margin-left: auto;
+    margin-right: auto;
 `;
 
-// Teraz, gdy ButtonProps jest poprawny, używamy go jawnie.
-const PrimaryButton = styled(Button)<ButtonProps>` 
-    background: linear-gradient(180deg, #17a2ff, #0a84c9);
+const PrimaryButton = styled(Button)<ButtonProps>`
+    //background: linear-gradient(180deg, #17a2ff, #0a84c9); // Possibly use this
     color: white;
-    padding: ${({theme}) => theme.spacing(1.5)};
     height: 64px;
     font-weight: 800;
-    box-shadow: 0 8px 20px rgba(0,0,0,0.45), inset 0 0 12px rgba(255,255,255,0.02);
-    &:hover { transform: translateY(-2px); }
+    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.45), inset 0 0 12px rgba(255, 255, 255, 0.02);
+
+    transition: all 0.25s ease;
+
+    &:hover {
+        box-shadow: 0 0 10px #17a2ff,
+        0 0 20px rgba(23, 162, 255, 0.4),
+        inset 0 0 20px rgba(255, 255, 255, 0.1);
+        transform: scale(1.005);
+    }
 `;
 
-// Jawne użycie ButtonProps
 const SecondaryButton = styled(Button)<ButtonProps>`
     border: 2px solid rgba(255,255,255,0.08);
-    padding: ${({theme}) => theme.spacing(1.25)};
     height: 56px;
     font-weight: 800;
     color: rgba(255,255,255,0.95);
+
+    transition: all 0.25s ease;
     &:hover {
-        border-color: #ffffff;
-        transform: translateY(-2px);
+        box-shadow:
+                0 0 10px #17a2ff,
+                0 0 20px rgba(23, 162, 255, 0.4),
+                inset 0 0 20px rgba(255,255,255,0.1);
+        transform: scale(1.005);
     }
 `;
