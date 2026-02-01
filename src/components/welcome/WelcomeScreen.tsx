3e12dc614e8789d050f89eff8b4072f78b5b0f8d
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2026-02-01 13:55:21 +0100

    Add version tag

--- src/components/welcome/WelcomeScreen.tsx
+++ src/components/welcome/WelcomeScreen.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import {Box} from '@mui/material';
+import {Box, Typography} from '@mui/material';
 import {styled} from '@mui/material/styles';
 import TitleLogo from "./TitleLogo.tsx";
 import MovingSubtitle from "./MovingSubtitle.tsx";
 import StartGameButton from "./StartGameButton.tsx";
 
-const WelcomeScreen: React.FC = () => {
+const WelcomeScreen = () => {
     return (
         <WelcomeContainer>
             <TitleLogo/>
             <MovingSubtitle/>
             <StartGameButton/>
+            <VersionTag>v1.0.5</VersionTag>
         </WelcomeContainer>
     );
 };
@@ -31,4 +31,13 @@ const WelcomeContainer = styled(Box)`
     linear-gradient(90deg, rgba(255, 255, 255, 0.03) 1px, transparent 1px);
     background-size: 50px 50px;
     box-shadow: inset 0 0 100px rgba(0, 0, 0, 0.5);
+`;
+
+const VersionTag = styled(Typography)`
+    position: absolute;
+    bottom: 1vh;
+    right: 1vw;
+    opacity: 0.4;
+    font-size: 1rem;
+    user-select: none; // User cannot select it by a mistake
 `;
