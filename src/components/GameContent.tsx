7da954d82d7cc84ed5c0bb28dabb57e88a6b8751
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2026-02-02 08:49:34 +0100

    Move Finish duel screen

--- src/components/GameContent.tsx
+++ src/components/GameContent.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {Container, GlobalStyles} from '@mui/material';
 import {useGameContext} from '../context/GameContext.tsx';
 import GameScreen from './duel/GameScreen.tsx';
-import FinishedScreen from './FinishedScreen.tsx';
 import FloorScreen from './floor/FloorScreen.tsx';
 import WelcomeScreen from './welcome/WelcomeScreen.tsx';
 import PodiumScreen from './podium/PodiumScreen.tsx';
+import FinishedScreen from "./duel/finish/FinishedScreen.tsx";
 
 const GameContent: React.FC = () => {
     const gameState = useGameContext().general.gameState;
