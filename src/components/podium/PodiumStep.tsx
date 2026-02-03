df6fe0c7f9edc6ebe4578f0aa730afe279a107d5
Grzegorz Puścizna <grzesiekpuscizna@gmail.com>
2026-02-03 13:09:23 +0100

    Improve Podium styling

--- src/components/podium/PodiumStep.tsx
+++ src/components/podium/PodiumStep.tsx
@@ -7,15 +7,21 @@ interface PodiumStepProps {
     player: PodiumPlayer
 }
 
-// TODO: FIX UI
 const PodiumStep = ({player}: PodiumStepProps) => {
     return (
         <Container key={player.position} podiumPosition={player.position}>
             <PositionText>{player.position}</PositionText>
             <PlayerNameText>{player.name}</PlayerNameText>
             <StatisticsContainer>
-                <StatisticsText>{`Wygrane pojedynki: ${player.duelsWon}`}</StatisticsText>
-                <StatisticsText>{`Złote kwadraty: ${player.timeBoostsUsed}`}</StatisticsText>
+                <StatisticLine>
+                    <StatisticsText>Wygrane pojedynki:</StatisticsText>
+                    <StatisticsNumber>{player.duelsWon}</StatisticsNumber>
+                </StatisticLine>
+
+                <StatisticLine>
+                    <StatisticsText>Złote kwadraty:</StatisticsText>
+                    <StatisticsNumber>{player.timeBoostsUsed}</StatisticsNumber>
+                </StatisticLine>
             </StatisticsContainer>
         </Container>
     );
@@ -37,7 +43,7 @@ const Container = styled(Box)<PodiumStepStyleProps>`
     position: relative;
     border-radius: 6px 6px 0 0;
     font-weight: 800;
-    text-transform: uppercase;
+    word-break: break-word;
     user-select: none; // User cannot select it by a mistake
 
     ${({podiumPosition}) => {
@@ -74,25 +80,38 @@ const PositionText = styled(Typography)`
 const PlayerNameText = styled(Typography)`
     font-size: 1.6rem;
     font-weight: 800;
+    color: #222;
     text-align: center;
     margin-top: 0.8rem;
     padding: 0 4px;
-    color: #222;
-    word-break: break-word;
     margin-bottom: 1.2rem;
 `;
 
 const StatisticsContainer = styled(Box)`
     position: absolute;
     bottom: 1rem;
-    opacity: 0.4;
-    font-size: 1rem;
+    justify-items: center;
+    text-transform: capitalize;
+    font-size: 1.4rem;
+`
+
+const StatisticLine = styled(Box)`
+    display: flex;
+    flex-direction: row;
 `
 
 const StatisticsText = styled(Typography)`
     font-size: 1.2rem;
-    text-align: center;
     color: #222;
-    opacity: 0.6;
-    word-break: break-word;
+    opacity: 0.5;
+    text-align: center;
+    margin-right: 0.3rem;
+`;
+
+const StatisticsNumber = styled(Typography)`
+    font-size: 1.2rem;
+    font-weight: 800;
+    color: #222;
+    opacity: 0.8;
+    text-align: center;
 `;
